155dac5892686433ee975df41bbf99c171538e1a
divaaga <93255055+divaaga@users.noreply.github.com>
2023-09-05 18:24:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+    save("my_filter_selfie.png");
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose_x = " + results[0].pose.nose.x);
+        console.log("nose_y = " + results[0].pose.nose.y);
+    }
+}
